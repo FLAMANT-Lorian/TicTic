f0dbe76fcec0fb77d2ec16a7320c3bfd0683279b
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-10-06 15:14:13 +0200

    Updated constant files

--- code/tictic/lib/screens/welcome/welcome.dart
+++ code/tictic/lib/screens/welcome/welcome.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:tictic/constants/colors.dart';
+import 'package:tictic/constants/durations.dart';
+import 'package:tictic/constants/fonts.dart';
+import 'package:tictic/constants/sizes.dart';
+import 'package:tictic/screens/welcome/widgets/bullet.dart';
 import 'package:tictic/screens/welcome/widgets/logo_welcome.dart';
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
   const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  final PageController _pageController = PageController(viewportFraction: 1);
+
+  int _currentIdx = 0;
+
+  final _items = [
+    'L’harmonie financière dans vos groupes, en toute simplicité !',
+    'Calculs instantanés, équité garantie avec TicTic !',
+    'Calculs fastidieux ? Non merci. Optez pour la simplicité avec TicTic !',
+    'TicTic : Vos dépenses partagées en toute simplicité !',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +32,7 @@ class Welcome extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/img/back1.png'),
+            image: AssetImage("assets/img/back1.png"),
             fit: BoxFit.cover,
           ),
         ),
@@ -19,7 +40,54 @@ class Welcome extends StatelessWidget {
           child: Column(
             children: [
               LogoWelcome(),
-              Text('Bonjour'),
+              SizedBox(height: kVerticalPadding),
+              SizedBox(
+                height: kTextSliderHeight,
+                child: PageView.builder(
+                  scrollDirection: Axis.horizontal,
+                  controller: _pageController,
+                  itemCount: _items.length,
+                  onPageChanged: (int index) {
+                    setState(() {
+                      _currentIdx = index;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: kHorizontalPadding,
+                      ),
+                      child: Text(_items[index], style: kTagLine, textAlign: TextAlign.center,),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: kVerticalPadding),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _items.map((item) {
+                  return GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    // Pour que le margin fonctionne comme le padding
+                    onTap: () {
+                      _pageController.animateToPage(
+                        _items.indexOf(item),
+                        duration: kAnimationDuration,
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                    child: Bullet(
+                      marginRight: _items.indexOf(item) == _items.length - 1
+                          ? 0
+                          : kHorizontalPaddingL,
+                      width: kSliderButtonWidth,
+                      color: _items.indexOf(item) == _currentIdx
+                          ? kMainColor
+                          : kSecondaryColor,
+                    ),
+                  );
+                }).toList(), // Pour convertir vers un véritable tableaux
+              ),
             ],
           ),
         ),
