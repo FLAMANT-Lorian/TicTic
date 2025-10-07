e88b9fcfe17a99e16cce06b38e8b72aba89d4cd3
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-10-07 14:01:56 +0200

    Implementation of internationalization

--- code/tictic/lib/main.dart
+++ code/tictic/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tictic/constants/colors.dart';
+import 'package:tictic/l10n/app_localizations.dart';
 import 'package:tictic/routes.dart';
 
 void main() {
@@ -12,8 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TicTic',
+      title: AppLocalizations.of(context)?.appTitle ?? 'TicTic', // Si je suis certain je met le !
+      // Nullsafety -> Prêter de l'attention au valeur null
       routes: routes,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate, // Material widgets
+        GlobalWidgetsLocalizations.delegate, // widgets génériques
+        GlobalCupertinoLocalizations.delegate, // iOS widgets
+      ],
+      supportedLocales: [const Locale('fr'), Locale('en')],
       theme: kAppTheme,
     );
   }
--- code/tictic/lib/screens/welcome/welcome.dart
+++ code/tictic/lib/screens/welcome/welcome.dart
@@ -1,40 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:tictic/constants/colors.dart';
 import 'package:tictic/constants/sizes.dart';
+import 'package:tictic/screens/scaffold_with_image.dart';
 import 'package:tictic/screens/welcome/widgets/call_to_actions.dart';
 import 'package:tictic/screens/welcome/widgets/logo_welcome.dart';
-import 'package:tictic/screens/welcome/widgets/text_divider.dart';
 import 'package:tictic/screens/welcome/widgets/text_slider_with_bullets.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
+
   static const String routeName = '/';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/img/back1.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              Spacer(),
-              LogoWelcome(),
-              Spacer(),
-              SizedBox(height: kVerticalPadding),
-              TextSliderWithBullets(),
-              Spacer(),
-              // Fait un espace au millieu avec toute la place restante (Spacer())
-              CallToActions(),
-            ],
-          ),
-        ),
+    return ScaffoldWithImage(
+      child: Column(
+        children: [
+          Spacer(),
+          LogoWelcome(),
+          Spacer(),
+          SizedBox(height: kVerticalPadding),
+          TextSliderWithBullets(),
+          Spacer(),
+          // Fait un espace au millieu avec toute la place restante (Spacer())
+          CallToActions(),
+        ],
       ),
     );
   }
